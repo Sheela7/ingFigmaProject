e1038ec70622b4a831c7f5a2d12922fb7f7b7fc2
sheela7 <sheela7>
2023-07-05 13:52:21 +0545

    worked with margines, paddings of overall responsive design, carousel for image added with jaavascript

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let navbar = document.querySelector(".navbar");
-i;
 let hamBurgerMenu = document.querySelector(".hamburger-menu");
 let hamBurgerMenuIcon = document.querySelector(".hamburger-menu i");
 
@@ -10,3 +9,36 @@ function showMenu() {
     ? "fa-solid fa-bars"
     : "fa-solid fa-xmark";
 }
+
+// .....
+const header = document.querySelector("header");
+const prevButton = document.querySelector("#prevButton");
+const nextButton = document.querySelector("#nextButton");
+
+const headerImages = [
+  "src/images/headerImages/forest.png",
+  "src/images/headerImages/forestNext.jpg",
+  "src/images/headerImages/forestPrev.jpg",
+];
+let currentImageIndex = 0;
+
+prevButton.addEventListener("click", () => {
+  console.log("pressed prev");
+  currentImageIndex =
+    (currentImageIndex - 1 + headerImages.length) % headerImages.length;
+  header.classList.add("slide");
+  setTimeout(() => {
+    header.style.backgroundImage = `url(${headerImages[currentImageIndex]})`;
+    header.classList.remove("slide");
+  }, 500);
+});
+
+nextButton.addEventListener("click", () => {
+  console.log("pressed next");
+  currentImageIndex = (currentImageIndex + 1) % headerImages.length;
+  header.classList.add("slide");
+  setTimeout(() => {
+    header.style.backgroundImage = `url(${headerImages[currentImageIndex]})`;
+    header.classList.remove("slide");
+  }, 500);
+});
